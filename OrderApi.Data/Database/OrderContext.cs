013353098964d71b3d4f8d260720de22167424d9
kakhiG <49832276+kakhiG@users.noreply.github.com>
2020-10-29 23:42:54 +0400

    Update OrderContext.cs

--- OrderApi.Data/Database/OrderContext.cs
+++ OrderApi.Data/Database/OrderContext.cs
@@ -20,21 +20,21 @@ namespace OrderApi.Data.Database
                     Id = Guid.Parse("9f35b48d-cb87-4783-bfdb-21e36012930a"),
                     OrderState = 1,
                     CustomerGuid = Guid.Parse("d3e3137e-ccc9-488c-9e89-50ba354738c2"),
-                    CustomerName = "buyer 1"
+                    CustomerName = "Customer1"
                 },
                 new Order
                 {
                     Id = Guid.Parse("bffcf83a-0224-4a7c-a278-5aae00a02c1e"),
                     OrderState = 1,
                     CustomerGuid = Guid.Parse("4a2f1e35-f527-4136-8b12-138a57e1ba08"),
-                    CustomerName = "buyer 2"
+                    CustomerName = "Customer 2"
                 },
                 new Order
                 {
                     Id = Guid.Parse("58e5cd7d-856b-4224-bdff-bd8f85bf5a6d"),
                     OrderState = 2,
                     CustomerGuid = Guid.Parse("334feb16-d7bb-4ca9-ab56-f4fadeb88d21"),
-                    CustomerName = "buyer 3"
+                    CustomerName = "Customer3"
                 }
             };
 
